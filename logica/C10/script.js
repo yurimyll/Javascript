a2eeabed9563abc0fff43ea26b9d83ee2e9202a0
yurimyll <123509489+yurimyll@users.noreply.github.com>
2024-07-04 12:24:09 -0300

    Add files via upload

--- logica/C10/script.js
+++ logica/C10/script.js
@@ -1,16 +1,73 @@
-var segredo = 5;
-var input = document.querySelector("input");
+function sorteia() {
 
-function verifica() {
+    return Math.round(Math.random() * 10);
 
-    if(input.value == segredo) {
-        alert("Você ACERTOUU")
-    }  else {
-        alert("Você ERROUU")
+ }
+
+ function sorteiaNumeros(quantidade) {
+
+     var segredos = [];
+
+     var numero = 1;
+
+     while(numero <= quantidade) {
+
+           var numeroAleatorio = sorteia();
+           var achou = false;
+
+           if (numeroAleatorio !== 0) {
+                  for(var posicao = 0; posicao < segredos.length; posicao++) {
+
+                        if(segredos[posicao] == numeroAleatorio){
+                             achou = true;
+                             break;
+                        }
+
+                  }
+
+                  if (achou == false) {
+                        segredos.push(numeroAleatorio);
+                        numero++;
+                  }
+           }
+
+     }
+
+     return segredos;
+
+ }
+
+ var segredos = sorteiaNumeros(3);
+
+ console.log(segredos);
+
+ var input = document.querySelector("input");
+ input.focus();
+
+ function verifica() {
+
+    var achou = false;
+
+    for(var posicao = 0; posicao < segredos.length; posicao++) {
+
+           if(input.value == segredos[posicao]) {
+
+                  alert("Você ACERTOU!");
+                  achou = true;
+                  break;
+           } 
     }
 
-}
+    if(achou == false) {
 
-var button = document.querySelector("button");
+           alert("Você ERROU!");
+    }
 
-button.onclick = verifica;
+    input.value = "";
+    input.focus();
+
+ }
+
+ var button = document.querySelector("button");
+
+ button.onclick = verifica;
